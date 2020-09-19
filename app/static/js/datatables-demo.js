3e41c536620ae446e7ced3fe01e49e66223ebcd9
unknown <mlgtech@MIL-BKFD-WS015.mlgcompanies.local>
2020-09-19 18:16:53 -0500

    removed old code design

--- app/static/js/datatables-demo.js
+++
@@ -1,8 +0,0 @@
-// Call the dataTables jQuery plugin
-$(document).ready(function() {
-  $('#dataTable').DataTable();
-});
-
-$('#dataTable > thead > tr > th').each(function(){
-    console.log("Hello world");
-})
